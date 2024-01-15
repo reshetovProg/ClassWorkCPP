65b67960f301fc2a4ce9ca7781f5a1640e87d527
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2024-01-15 19:39:24 +0300

    Add files via upload

---
+++ 15.01.2024/DefaultClass.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+public:
+    Student(string name, int age) {
+        this->age = age;
+        this->name = name;
+    }
+    void show() {
+        cout << "name: " << name << " age: " << age<<endl;
+    }
+};
+
+int main()
+{
+    Student st1("Pol", 23);
+    st1.show();
+}
+
