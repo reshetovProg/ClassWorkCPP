da94bf15829801ec37677a8841161e99907033ae
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2024-01-15 21:16:50 +0300

    Add files via upload

--- 15.01.2024/DefaultClass.cpp
+++ 15.01.2024/DefaultClass.cpp
@@ -1,23 +1,74 @@
 ﻿#include <iostream>
 using namespace std;
 
-class Student {
+/*создать класс Дробь
+Объект данного класса должен состоять из
+числителя и знаменателя в обязательном порядке
+Реализовать метод вывода дроби в виде:
+3/6
+*/
+class Fraction {
 private:
-    string name;
-    int age;
+	int total;
+	int num;
+	int denum;
 public:
-    Student(string name, int age) {
-        this->age = age;
-        this->name = name;
-    }
-    void show() {
-        cout << "name: " << name << " age: " << age<<endl;
-    }
+	Fraction(int num, int denum) {
+		total = num / denum;			
+		this->num = num % denum;
+		this->denum = denum;
+	}
+	void show() {
+		if(total==0)
+			cout << num << "/" << denum<<endl;
+		else
+			cout <<total<<" "<< num << "/" << denum<<endl;
+	}
 };
+/*
+студент: номер группы, имя, ср. балл
+методы:
+выводить всю информацию о студенте
+определять является ли студент отличником, хор
+троечником или двоечник. (возвращает строку)
+*/
+class Student {
+	string group;
+	string name;
+	float avg;
+public:
+	Student(string group, string name, float avg) {
+		this->group = group;
+		this->name = name;
+		this->avg = avg;
+	}
+	void show() {
+		cout << group << " : " << name << " : " << avg<<endl;
+	}
+	string getStatus() {
+		if (avg > 4.6) return "A";
+		if (avg > 3.6) return "B";
+		if (avg > 2.6) return "C";
+		else return "D";
+	}
+};
+
 
 int main()
 {
-    Student st1("Pol", 23);
-    st1.show();
+	//Student st1("34", "Pol", 4.5);
+	//Student st2("345", "Pol2", 3.5);
+	//Student st3("3", "Pol3", 2.5);
+	//st1.show();
+	//st2.show();
+	//st3.show();
+	//cout << st1.getStatus()<<endl;
+	//cout << st2.getStatus() << endl;
+	//cout << st3.getStatus() << endl;
+	Fraction f1(4, 7);
+	Fraction f2(23, 10);
+	f1.show();
+	f2.show();
+
 }
 
